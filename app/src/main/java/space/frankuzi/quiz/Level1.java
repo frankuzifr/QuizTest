cac6aef98f367280330d75db41718a6c19495b3e
frankuzifr <wasabi555579@gmail.com>
2021-12-23 00:57:15 +0300

    Created dialog window

--- app/src/main/java/space/frankuzi/quiz/Level1.java
+++ app/src/main/java/space/frankuzi/quiz/Level1.java
@@ -1,12 +1,24 @@
 package space.frankuzi.quiz;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.logging.Level;
 
 public class Level1 extends AppCompatActivity {
+
+    private Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +33,41 @@ public class Level1 extends AppCompatActivity {
 
         Window window = getWindow();
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.previewdialog);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setCancelable(false);
+
+        TextView buttonClose = dialog.findViewById(R.id.buttonClose);
+        buttonClose.setOnClickListener(v -> {
+            try {
+                Intent intent = new Intent(Level1.this, GameLevels.class);
+                startActivity(intent);
+                finish();
+            }catch (Exception e){
+
+            }
+            dialog.dismiss();
+        });
+
+        Button buttonContinue = dialog.findViewById(R.id.buttonContinue);
+        buttonContinue.setOnClickListener(v -> {
+            try{
+                dialog.dismiss();
+            }catch (Exception e){
+
+            }
+        });
+
+        dialog.show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Level1.this, GameLevels.class);
+        startActivity(intent);
+        finish();
     }
 }
